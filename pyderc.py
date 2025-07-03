3f2a0743b0d4c42ee478e186cb3ba4c23ef3e4f4
yrgo28 <yaarago2807@gmail.com>
2025-07-02 22:53:11 -0400

    Add files via upload

---
+++ pyderc.py
@@ -0,0 +1,11 @@
+import os
+
+#PYDE CONFIG FILE
+#ALL CONFIGS MADE HERE, WILL BE LOADED
+
+has_wd = False #is workspace loaded?
+FIRST_TIME_OPTIONS = True #show note in options menu
+show_wd_main = False #show working directory in main menu
+workspace = #"/path/to/wd" #current workspace
+char_separators = "=" #character work as separator line
+
