560eca9ad8f21d2aad092af245848bd01a37370f
yrgo28 <yaarago2807@gmail.com>
2025-07-02 23:01:36 -0400

    Update pyderc.py

--- pyderc.py
+++ pyderc.py
@@ -6,6 +6,6 @@ import os
 has_wd = False #is workspace loaded?
 FIRST_TIME_OPTIONS = True #show note in options menu
 show_wd_main = False #show working directory in main menu
-workspace = #"/path/to/wd" #current workspace
+workspace = None #"/path/to/wd" #current workspace
 char_separators = "=" #character work as separator line
 
